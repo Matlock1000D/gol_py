83d13e0e51d8a7440a4ca1c887cf36b0dd052deb
lpjapp@gmail.com <lpjapp@gmail.com>
2022-11-21 17:00:45 +0200

    Projektin ja koodin siistimistä

--- src/gol.py
+++ src/gol.py
@@ -1,19 +1,25 @@
+"""Conwayn Game of Life"""
+
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.animation import FuncAnimation
 
-class gol_main:
+class GolMain:
+    """Game of Life -peliä kuvaava luokka"""
 
     testvar = 0
+    #Laita ykköseksi, jos systeemi on löytänyt kiintopisteen
 
     def update_state(self):
+        """Iteroi pelikenttää yksi kierros eteenpäin."""
         newfield = [[0] * self.maxx for i in range(self.maxx)]
         for x in range(self.maxx):
             for y in range(self.maxx):
                 lifeval = 0
                 for i in range (x-1,x+2):
                     for j in range (y-1,y+2):
-                        if ((i < 0 or i >= self.maxx or j < 0 or j >= self.maxx) or (i == x and j == y)):
+                        if ((i < 0 or i >= self.maxx or j < 0 or j >= self.maxx) 
+                        or (i == x and j == y)):
                             value = 0
                         else:
                             value = self.field[j][i]
@@ -30,6 +36,7 @@ class gol_main:
         return newfield
 
     def printfield(self, fieldmap):
+        """Tulosta kentän tila"""
         self.axis.clear()
         self.axis = plt.axes(xlim =(0,self.screensize),
                 ylim =(0,self.screensize))
@@ -44,21 +51,25 @@ class gol_main:
                     self.axis.add_patch(Rectangle((x,y),block,block))
     
     def frames(self):
+        """Apufunktio animaation piirtoon... Mutta hoitaa tosiasiassa myös varsinaisen pelin pyörittämisen!"""
+        self.steady_state = 1
         yield self.field
         for i in range(self.rounds):
+            print(i)
             yield self.update_state()
+            if self.steady_state == 1:
+                break
 
     def run_game(self, maxrounds):
         self.rounds = maxrounds
         anim = FuncAnimation(self.fig, self.printfield, frames=self.frames, interval=500)
         plt.show()
-        #anim.save("Keek.gif")
         if self.steady_state == 1:
             return -1
         return 1
-
-    
+   
     def __init__(self, size, initstate):
+        self.rounds = -1
         self.field = [[0] * size for i in range(0,size)]
         self.maxx = size
         self.testvar = 1
@@ -70,7 +81,8 @@ class gol_main:
         self.steady_state = 0
 
         #kuvien alustaminen
-        self.fig = plt.figure()
-        self.axis = plt.axes()
+        self.fig = plt.figure(frameon=False)
+        self.axis = plt.axes(frame_on=False)
         #self.axis.get_xaxis().set_ticks([])
         #self.axis.get_yaxis().set_ticks([])
+        
--- test/gol_test.py
+++ test/gol_test.py
@@ -1,15 +1,19 @@
-import gol
+import sys
 import unittest
+sys.path.append("src")
+sys.path.append(r'..\src')
+import gol
+
 
 class Tests(unittest.TestCase):
     def test_test(self):
         self.assertEqual(True,True)
     def test_init(self):
-        testgol = gol.gol_main(5, [])
+        testgol = gol.GolMain(5, [])
         self.assertEqual(testgol.testvar,1)
     def test_init_nontrivial(self):
         size = 3
-        testgol = gol.gol_main(size, [(1,1)])
+        testgol = gol.GolMain(size, [(1,1)])
         result = [0 for i in range(size)]
         result[0] = '000'
         result[1] = '010'
@@ -19,11 +23,11 @@ class Tests(unittest.TestCase):
         self.assertEqual(testgol.field,result)
     def test_running_dead(self):
         size = 3
-        testgol = gol.gol_main(size, [(1,1)])
+        testgol = gol.GolMain(size, [(1,1)])
         self.assertEqual(testgol.run_game(10),-1)  
     def test_running_glider(self):
         size = 100
-        testgol = gol.gol_main(size, [(50,51),(50,50),(50,49),(49,49),(48,50)])
+        testgol = gol.GolMain(size, [(50,51),(50,50),(50,49),(49,49),(48,50)])
         self.assertEqual(testgol.run_game(100),1)
 
 if __name__ == '__main__':
